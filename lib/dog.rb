fbc9442305e77770ac79b21c955d0979f9fc7e5b
Ashley <ashleynjeri7@gmail.com>
2022-10-23 18:10:52 +0300

    passed tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,20 @@
 class Dog
+    # setter method
     def name=(dog_name)
         @name = dog_name
     end
+
+    # getter method for instance variable @name
     def name
         @name
     end
 
+     # setter method
     def breed=(dog_breed)
-        @breed=dog_breed
+        @breed = dog_breed
     end
+
+    # getter method for instance variable @breed
     def breed
         @breed
     end
--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,20 @@
 class Person
+    # setter method
     def name=(person_name)
         @name=person_name
     end
+
+    # getter method for instance variable @name
     def name
         @name
     end
+
+     # setter method
     def job=(person_job)
         @job = person_job
     end
+
+    # getter method for instance variable @job
     def job
         @job
     end
